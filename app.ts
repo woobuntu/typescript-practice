00119230c8d1f845f32c102ef20674619f8f1bec
woobuntu <boutime2017@gmail.com>
2021-07-17 16:13:32 +0900

    함수 타입

--- app.ts
+++ app.ts
@@ -10,4 +10,22 @@ function printResult(num: number) {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  // cb의 return type이 void임에도 막상 전달되는 함수가 특정 값을 반환해도 타입스크립트는 에러로 인식하지 않는다.
+  // 여기서 void는 반환 값이 없다는 의미라기 보다는 반환값을 무시한다는 의미에 가깝기 때문
+  // cb의 반환값으로 무언가를 하지 않는다는 것
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+// 함수 선언문의 타입을 화살표 함수(표현식)으로 지정할 수 있다니...
+
+combineValues = add;
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  return 3;
+});
