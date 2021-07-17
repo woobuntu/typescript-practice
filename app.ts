7a009ba5b0c5919b71628b82d7982c42eb9f99d7
woobuntu <boutime2017@gmail.com>
2021-07-17 16:42:56 +0900

    unknown과 never

--- app.ts
+++ app.ts
@@ -1,31 +1,25 @@
-function add(n1: number, n2: number) {
-  // 일반적으로는 타입스크립트가 타입 추론을 하게끔 return type을 명시하지 않는다고 한다.
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+// any 타입과 마찬가지로 어떤 값이든 할당할 수 있다.
+if (typeof userInput === "string") {
+  // 하지만 any와는 다르게 이와 같은 동적 체크 없이는 바로 할당할 수 없다.
+  userName = userInput;
+}
+// 이처럼 unknown은 any보다는 restrictive하기 때문에 any보다 유용하다
+// 물론 이 경우에도 userInput을 string과 number의 union으로 지정하는 게 더 나아 보인다.
+
+function generateError(message: string, code: number): never {
+  throw {
+    message,
+    errorCode: code,
+  };
+
+  // error 반환은 스크립트 자체를 블락하기 때문에 반환 타입을 never로 명시해주는 것이 좋다.
+  // 초기 버전의 타입스크립트에서는 never라는 타입이 없었기에 명시해주지 않으면 void로 타입 추론이 되기 때문
+  // while (true) {} 이와 같은 무한 반복문도 never의 예시 중 하나이다.
 }
 
-function printResult(num: number) {
-  // 함수가 아무것도 반환하지 않을 때의 타입을 명시하고자 한다면 void를 사용해야 한다.
-  // undefined type은 변수에는 적용 가능하지만 함수의 반환 타입으로는 사용할 수 없다.
-  // return; 또는 return undefined;를 명시하면 사용할 수 있지만 굳이...
-  console.log("Result: " + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  // cb의 return type이 void임에도 막상 전달되는 함수가 특정 값을 반환해도 타입스크립트는 에러로 인식하지 않는다.
-  // 여기서 void는 반환 값이 없다는 의미라기 보다는 반환값을 무시한다는 의미에 가깝기 때문
-  // cb의 반환값으로 무언가를 하지 않는다는 것
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combineValues: (a: number, b: number) => number;
-// 함수 선언문의 타입을 화살표 함수(표현식)으로 지정할 수 있다니...
-
-combineValues = add;
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-  return 3;
-});
+generateError("An error occurred!", 500);
