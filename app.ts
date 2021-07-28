0777a0872d9d420127b442783fc12b6983b407db
in uh, lee <boutime2017@gmail.com>
2021-07-28 13:24:27 +0900

    추상 클래스

--- app.ts
+++ app.ts
@@ -1,25 +1,26 @@
-class Department {
-  protected employees: string[] = [];
-  // 이렇게 접근자를 키워드로 설정해주는 것은 자바스크립트에서는 지원하지 않는 기능이다.
-  // private으로 하면 상속 클래스에서는 접근할 수 없기 때문에 protected
-
+// 이와 같은 추상 클래스는 스스로 인스턴스를 생성할 수 없고,
+// 이러한 추상 클래스를 상속한 자식 클래스만이 인스턴스를 생성할 수 있다.
+abstract class Department {
   static fiscalYear: number = 2020;
 
-  constructor(private readonly id: string, private name: string) {}
+  constructor(
+    protected readonly id: string,
+    private name: string,
+    protected employees: string[] = []
+  ) {}
   // field의 선언과 생성자에서의 초기화를 합친 문법으로 자바스크립트에서는 지원하지 않는다.
   // readonly키워드 역시 자바스크립트에서는 지원하지 않는다.
+  // private으로 하면 상속 클래스에서는 접근할 수 없기 때문에 protected
 
   // 인스턴스가 아닌 클래스를 매개로 호출하는 static 메서드
   static createEmployee(name: string) {
     return { name };
   }
 
-  // 파라미터로 this의 타입을 명시해주는 것은
-  // describe메소드가 Department의 구조를 따르는 객체에서만 호출되도록 하는 안전장치라고 볼 수 있다.
-  // 꼭 Department의 인스턴스일 필요는 없음
-  describe(this: Department) {
-    console.log(`Department(${this.id}): ${this.name}`);
-  }
+  // 이 Department라는 추상 클래스를 상속하는 자식 클래스는
+  // return값이 없는 describe메소드를 '반드시' 구현해야 한다.
+  // 또한 자식 클래스에서의 this는 Department클래스에 기반한 클래스면 무엇이든 가능하다.
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -49,6 +50,10 @@ class ITDepartment extends Department {
   set mostRecentAdmin(value: string) {
     if (!value) throw new Error("관리자 이름을 인자로 넘겨야 합니다.");
     this.addEmployee(value);
+  }
+
+  describe(this: ITDepartment) {
+    console.log("IT Department ID : ", this.id);
   }
 
   addEmployee(name: string) {
