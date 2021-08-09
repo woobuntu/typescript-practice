ef90d3b80ee0626b124c5d4c6f23a93c607ff2f1
in uh, lee <boutime2017@gmail.com>
2021-08-09 14:25:51 +0900

    typeof, in, instanceof를 이용한 type guard
    Union타입의 유연성?

--- intersections.ts
+++ intersections.ts
@@ -39,3 +39,52 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 // 교집합이 number이므로 Universal의 타입은 number이다.
+
+function add(a: Combinable, b: Combinable) {
+  // typeof를 사용한 type guard
+  return typeof a === "string" || typeof b === "string"
+    ? a.toString() + b.toString()
+    : a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(employee: UnknownEmployee) {
+  console.log(`Name : ${employee.name}`);
+  // in문을 이용한 type guard
+  if ("privileges" in employee)
+    console.log(`privileges : ${employee.privileges}`);
+  if ("startDate" in employee) console.log(`startDate : ${employee.startDate}`);
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo ... ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // class의 경우 in보다는 instanceof type guard가 바람직하다.
+  if (vehicle instanceof Truck) vehicle.loadCargo(1000);
+}
+
+useVehicle(v1);
+useVehicle(v2);
