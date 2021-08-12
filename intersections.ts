82ad56af0bb6f4706c84081857eed8a3fedcb8da
in uh, lee <boutime2017@gmail.com>
2021-08-12 15:43:47 +0900

    type casting

--- intersections.ts
+++ intersections.ts
@@ -115,3 +115,22 @@ function moveAnimal(animal: Animal) {
   }
   console.log(`Moving with speed : ${speed}`);
 }
+
+// 아래의 두 방법으로 type casting이 가능하지만 첫번째 방법은 jsx와는 양립할 수 없다.
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+// const userInputElement = document.getElementById(
+//   "user-input"
+// )! as HTMLInputElement;
+// 타입스크립트가 html파일까지 관찰하지 않기 때문에 이 element가 input태그임을 미리 알지는 못한다.
+// 따라서 type casting 없이는 userInputElement의 value에 접근한다던가 하는 작업을 할 수 없다.
+// 하지만 개발자인 우리는 알고 있으므로, 개발자의 책임 하에 type casting을 하는 것
+// !는 절대 null값일 수 없다는 것을 의미한다.
+
+// userInputElement.value = "Hi there!";
+
+// 만약 userInputElement가 반드시 null이 아니라고 장담할 수 없을 경우 아래와 같은 방법을 사용할 수도 있다.
+const userInputElement = document.getElementById("user-input");
+if (userInputElement)
+  (userInputElement as HTMLInputElement).value = "Hi there!";
