7f0fc2ea02ca9d352c3c417dab2f86cd430e9380
in uh, lee <boutime2017@gmail.com>
2021-08-24 16:06:38 +0900

    generic function 생성

--- generics.ts
+++ generics.ts
@@ -9,3 +9,12 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then((data) => {
   data.split(" ");
 });
+
+// T와 U의 intersection
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+
+mergedObj.name;
