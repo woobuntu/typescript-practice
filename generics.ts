f848990442393bcbad2dcb7faf858d72e902c286
in uh, lee <boutime2017@gmail.com>
2022-03-12 13:59:13 +0900

    generic classes

--- generics.ts
+++ generics.ts
@@ -41,3 +41,22 @@ function extractAndConvert<T extends object, U extends keyof T>(
 ) {
   return obj[key];
 }
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    // removeItem의 경우 참조형 데이터에는 정상 동작하지 않는다.
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
