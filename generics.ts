54ccc3887be6e0009e02dc5facf7bdd0aaf61f88
in uh, lee <boutime2017@gmail.com>
2022-03-12 13:26:40 +0900

    another generic function example

--- generics.ts
+++ generics.ts
@@ -17,3 +17,20 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 }
 
 // const mergedObj = merge({ name: "Max" }, 30);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = `Got 1 element`;
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
