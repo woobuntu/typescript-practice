0ea1a347741bd956b324c73a26223a74cff446b9
in uh, lee <boutime2017@gmail.com>
2022-03-12 13:31:01 +0900

    keyof constraint

--- generics.ts
+++ generics.ts
@@ -34,3 +34,10 @@ function countAndDescribe<T extends Lengthy>(element: T) {
 }
 
 console.log(countAndDescribe("Hi there!"));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
